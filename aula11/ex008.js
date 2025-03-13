88605a4b527b882a148743afcf22f295bb186bff
Augusto Simionato <augusto.simionato2606@gmail.com>
2025-03-13 17:10:40 -0300

    Condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 94
+console.log(`A velociade do seu carro é ${vel}km/h.`)
+if(vel>80){
+    console.log('Você está acima da velocidade = infração + pontos na carteira.')
+} //condição simples
+console.log('Dirija sempre usando o cinto de segurança.')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Se você mora no país: ${pais} singifica que pra mim você é:`)
+if(pais == 'Brasil'){
+    console.log('Brasileiro!')
+}else{
+    console.log('Estrangeiro!')
+}
